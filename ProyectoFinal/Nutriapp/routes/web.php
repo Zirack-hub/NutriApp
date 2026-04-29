23a81644720d0bb27ca14928c96c9721b5693bc5
martin <martinportelasegui@gmail.com>
2026-04-29 12:34:35 +0200

    Revert "Inicio de mostrar alimentos"
    This reverts commit 675821287f8cf1a11181cfe9fafeaadf7f189929.
    
    Revert para mi local

--- ProyectoFinal/Nutriapp/routes/web.php
+++ ProyectoFinal/Nutriapp/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\AlimentosController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthController::class, 'showLogin'])->name('showlogin');
@@ -12,4 +11,3 @@ Route::get('/usuarios', [UserController::class, 'usuarios'])->name('usuarios');
 Route::get('/usuarios/create', [UserController::class, 'create'])->name('usuarios.create');
 Route::post('/usuarios', [UserController::class, 'store'])->name('usuarios.store');
 Route::post('/usuarios/{usuario}/cambiar-password', [UserController::class, 'cambiarPassword'])->name('usuarios.cambiar-password');
-Route::get('/alimentos', [AlimentosController::class,'mostrar'])->name('alimentos');
